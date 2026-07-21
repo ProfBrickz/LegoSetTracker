53e35bd288e50e2eb89b0cd81b705cc85b28f089
ProfBrickz <79584392+ProfBrickz@users.noreply.github.com>
2026-07-21 13:07:15 -0400

    Fixed database migration

--- drizzle/0001_supreme_tarot.sql
+++ drizzle/0001_supreme_tarot.sql
@@ -5,7 +5,7 @@ CREATE TABLE "stickered_lego_pieces" (
 --> statement-breakpoint
 ALTER TABLE "stickered_lego_pieces" ADD CONSTRAINT "stickered_lego_pieces_base_piece_id_lego_pieces_id_fk" FOREIGN KEY ("base_piece_id") REFERENCES "public"."lego_pieces"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "stickered_lego_pieces" ADD CONSTRAINT "stickered_lego_pieces_stickered_piece_id_lego_pieces_id_fk" FOREIGN KEY ("stickered_piece_id") REFERENCES "public"."lego_pieces"("id") ON DELETE no action ON UPDATE no action;
-
+--> statement-breakpoint
 -- Add stickered lego pieces
 INSERT INTO stickered_lego_pieces (base_piece_id, stickered_piece_id)
 SELECT
